1c5d0c836839880f43c6f3c9406cef4305e23496
jalrax <trymental@gmail.com>
2018-04-10 17:53:27 +0300

    Added styles and dropdown

--- src/app/core/pages/wares-list/wares-list.component.ts
+++ src/app/core/pages/wares-list/wares-list.component.ts
@@ -7,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaresListComponent implements OnInit {
 
+  public list = [
+    {
+      name: 'foo',
+      price: 123,
+      picture: '/assets/adminlte/user1-128x128.jpg'
+    },
+    {
+      name: 'foytho',
+      price: 1223,
+      picture: '/assets/adminlte/user1-128x128.jpg'
+    },
+    {
+      name: 'ghjgh',
+      price: 222,
+      picture: '/assets/adminlte/user1-128x128.jpg'
+    },
+    {
+      name: 'fhjghjoo',
+      price: 122223,
+      picture: '/assets/adminlte/user1-128x128.jpg'
+    },
+    {
+      name: 'dfvdfv',
+      price: 123333,
+      picture: '/assets/adminlte/user1-128x128.jpg'
+    }
+  ];
+
+  date = new Date();
+
   constructor() {
   }
 
